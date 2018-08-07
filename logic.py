032f18ab329f7b67f361448fb37f377be3f73083
Jaroslaw Nowakowski <nowakowski.priv@gmail.com>
2018-08-07 08:02:08 +0200

    working buttons to Previous & Next 10 Planets

--- logic.py
+++ logic.py
@@ -4,6 +4,18 @@ import json
 # connect to SWAPI server and get 10 planets data
 # gives back dictionary
 def giveMeTenPlanets():
-    url = "https://swapi.co/api/planets/?page=1"
+    url = "https://swapi.co/api/planets/?page=2"
     tenPlanets = requests.get(url).json()
     return tenPlanets
+
+def giveMeTenPreviousPlanets(urlTo10PreviousPlanets):
+    url = urlTo10PreviousPlanets
+    tenPreviousPlanets = requests.get(url).json()
+    return tenPreviousPlanets
+
+
+
+def giveMeTenNextPlanets(urlTo10NextPlanets):
+    url = urlTo10NextPlanets
+    tenNextPlanets = requests.get(url).json()
+    return tenNextPlanets
--- main.py
+++ main.py
@@ -1,12 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import logic
 
 app = Flask(__name__)
 
+# main route to main page with Page1 (10 planets)
 @app.route('/')
 def index():
     tenPlanets = logic.giveMeTenPlanets()
     return render_template('planets.html', tenPlanets = tenPlanets)
+
+# route to 10 Previous Planets (Page + (-1) )
+@app.route('/previous')
+def previous():
+    urlTo10PreviousPlanets = request.args.get('previous')
+    tenPreviousPlanets = logic.giveMeTenPreviousPlanets(urlTo10PreviousPlanets)
+    return render_template('planets.html', tenPlanets = tenPreviousPlanets)
+
+# route to 10 Next Planets (Page + 1 )
+@app.route('/next')
+def next():
+    urlTo10NextPlanets = request.args.get('next')
+    tenNextPlanets = logic.giveMeTenNextPlanets(urlTo10NextPlanets)
+    return render_template('planets.html', tenPlanets = tenNextPlanets)   
+
 
 
 if __name__ == '__main__':
