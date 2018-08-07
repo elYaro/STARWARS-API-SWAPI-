3a668e3f23e1d471e5a517d59f499d6b281babaa
Jaroslaw Nowakowski <nowakowski.priv@gmail.com>
2018-08-07 08:21:20 +0200

    previous10planets

--- logic.py
+++ logic.py
@@ -8,14 +8,10 @@ def giveMeTenPlanets():
     tenPlanets = requests.get(url).json()
     return tenPlanets
 
+
+# connect to SWAPI server and get 10 previous planets data
+# gives back dictionary
 def giveMeTenPreviousPlanets(urlTo10PreviousPlanets):
     url = urlTo10PreviousPlanets
     tenPreviousPlanets = requests.get(url).json()
     return tenPreviousPlanets
-
-
-
-def giveMeTenNextPlanets(urlTo10NextPlanets):
-    url = urlTo10NextPlanets
-    tenNextPlanets = requests.get(url).json()
-    return tenNextPlanets
--- main.py
+++ main.py
@@ -9,19 +9,13 @@ def index():
     tenPlanets = logic.giveMeTenPlanets()
     return render_template('planets.html', tenPlanets = tenPlanets)
 
+
 # route to 10 Previous Planets (Page + (-1) )
 @app.route('/previous')
 def previous():
     urlTo10PreviousPlanets = request.args.get('previous')
     tenPreviousPlanets = logic.giveMeTenPreviousPlanets(urlTo10PreviousPlanets)
     return render_template('planets.html', tenPlanets = tenPreviousPlanets)
-
-# route to 10 Next Planets (Page + 1 )
-@app.route('/next')
-def next():
-    urlTo10NextPlanets = request.args.get('next')
-    tenNextPlanets = logic.giveMeTenNextPlanets(urlTo10NextPlanets)
-    return render_template('planets.html', tenPlanets = tenNextPlanets)   
 
 
 
